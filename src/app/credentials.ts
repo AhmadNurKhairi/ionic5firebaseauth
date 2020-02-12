618be647e848e4658f052caac707896cf69ec0fe
Bahit Hamid <bahit.hamid@gmail.com>
2020-02-12 20:26:49 +0800

    Added firebase credential and setup constructor

---
+++ src/app/credentials.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA6nePQJgHjG0CPgDANiaqKG6EilWyUflc",
+    authDomain: "ionic5firebaseauth.firebaseapp.com",
+    databaseURL: "https://ionic5firebaseauth.firebaseio.com",
+    projectId: "ionic5firebaseauth",
+    storageBucket: "ionic5firebaseauth.appspot.com",
+    messagingSenderId: "490970036957",
+    appId: "1:490970036957:web:0aac4df78ef76b8203b934"
+};

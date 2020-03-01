88655ce45eacc15b502aecabbce6fae8af1ccf09
Bahit Hamid <bahit.hamid@gmail.com>
2020-03-02 00:10:55 +0800

    Finishing touches on Dashboard page

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../services/user/auth.service";
+import { LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.page.html',
-  styleUrls: ['./dashboard.page.scss'],
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.page.html",
+  styleUrls: ["./dashboard.page.scss"]
 })
 export class DashboardPage implements OnInit {
+  public loading: HTMLIonLoadingElement;
+  constructor(
+    private authService: AuthService,
+    public loadingCtrl: LoadingController,
+    private router: Router,
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  async logoutUser(): Promise<void> {
+    this.loading = await this.loadingCtrl.create();
+    await this.loading.present();
+    this.authService.logoutUser().then(
+      () => {
+        this.loading.dismiss().then(() => {
+          this.router.navigateByUrl("login");
+        });
+      });
   }
-
 }

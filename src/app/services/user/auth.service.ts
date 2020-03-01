37ab09a3f696c4bd41dee6671acc4e22c6e6db3f
Bahit Hamid <bahit.hamid@gmail.com>
2020-03-01 22:06:34 +0800

    Modifying Auth Service

--- src/app/services/user/auth.service.ts
+++ src/app/services/user/auth.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from "@angular/core";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 @Injectable({
   providedIn: "root"
@@ -6,4 +9,37 @@ import { Injectable } from "@angular/core";
 export class AuthService {
   constructor() {}
 
+  loginUser(
+    email: string,
+    password: string
+  ): Promise<firebase.auth.UserCredential> {
+    return firebase.auth().signInWithEmailAndPassword(email, password);
+  }
+
+  signupUser(email: string, password: string): Promise<any> {
+    return firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((newUserCredential: firebase.auth.UserCredential) => {
+        // The code below actually creates a new document inside Firestore with a collection
+        // called 'userProfile' and inside the document named the user's UID and value of the
+        // user's email.
+        firebase
+          .firestore()
+          .doc(`/userProfile/${newUserCredential.user.uid}`)
+          .set({ email });
+      })
+      .catch(error => {
+        console.error(error);
+        throw new Error(error);
+      });
+  }
+
+  resetPassword(email: string): Promise<void> {
+    return firebase.auth().sendPasswordResetEmail(email);
+  }
+
+  logoutUser(): Promise<void> {
+    return firebase.auth().signOut();
+  }
 }

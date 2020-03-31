57a688fbdb67dd4b47809cb94acf7e559e235ed2
Bahit Hamid <bahit.hamid@gmail.com>
2020-03-31 09:49:53 +0800

    Adding httpclientmodule

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class RestService {
-
-  constructor() { }
+  constructor(public http: HttpClient) {}
 }

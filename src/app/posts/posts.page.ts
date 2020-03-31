49e8fcd9abc66255b83aec0d3599467f7c06f6c9
Bahit Hamid <bahit.hamid@gmail.com>
2020-03-31 09:55:19 +0800

    Implement getUsers into Posts page

--- src/app/posts/posts.page.ts
+++ src/app/posts/posts.page.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { RestService } from "../services/rest.service";
 
 @Component({
-  selector: 'app-posts',
-  templateUrl: './posts.page.html',
-  styleUrls: ['./posts.page.scss'],
+  selector: "app-posts",
+  templateUrl: "./posts.page.html",
+  styleUrls: ["./posts.page.scss"]
 })
 export class PostsPage implements OnInit {
+  posts: any;
 
-  constructor() { }
+  constructor(public restService: RestService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  getPosts() {
+    this.restService.getPosts().then(data => {
+      this.posts = data;
+      console.log(this.posts);
+    });
   }
-
 }

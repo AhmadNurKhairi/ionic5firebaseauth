51fac3d8fba052a91e60ef36f8139cb2d3f24b08
Bahit Hamid <bahit.hamid@gmail.com>
2020-03-31 09:52:59 +0800

    Add getPosts to rest service

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -5,5 +5,20 @@ import { HttpClient } from "@angular/common/http";
   providedIn: "root"
 })
 export class RestService {
+  apiUrl = "http://128.199.129.234";
+
   constructor(public http: HttpClient) {}
+
+  getPosts() {
+    return new Promise(resolve => {
+      this.http.get(this.apiUrl + "/posts").subscribe(
+        data => {
+          resolve(data);
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    });
+  }
 }
